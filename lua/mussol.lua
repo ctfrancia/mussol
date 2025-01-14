37e39dba113c57a13be5f98e0a3f284ceb8188ba
Christian Francia <christian.francia@tui.com>
2025-01-14 15:08:04 +0100

    show buffer in :buffers

--- lua/mussol.lua
+++ lua/mussol.lua
@@ -1,1 +1,20 @@
-print("hellow from mussol.lua")
+local function create_buffer()
+  local buf = vim.api.nvim_create_buf(true, true)
+  vim.api.nvim_buf_set_name(buf, "*scratch*")
+  vim.api.nvim_set_option_value("filetype", "lua", { buf = buf })
+  return buf
+end
+
+
+local function main()
+    local buf = create_buffer()
+  print("Hello from our plugin!!!!!!!!")
+end
+
+local function setup()
+  local augroup = vim.api.nvim_create_augroup("MUSSOL", { clear = true })
+  vim.api.nvim_create_autocmd("VimEnter",
+    { group = augroup, desc = "Set a fennel scratch buffer on load", once = true, callback = main })
+end
+
+return { setup = setup }
